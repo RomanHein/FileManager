454a324184933546272d3fb59baf80e4520a6263
Roman Hein <roman.hein.2008@gmail.com>
2026-01-08 16:35:17 +0100

    Added comments.

--- file_manager.h
+++ file_manager.h
@@ -15,6 +15,7 @@ private:
 	size_t _appended = 0;
 	bool _modified = false;
 
+	// Frees memory by letting go of unused values in _content.
 	void _compact() {
 		if (_mapping.empty()) {
 			_content.clear();
@@ -32,16 +33,6 @@ private:
 
 		_mapping.resize(_content.size());
 		std::iota(_mapping.begin(), _mapping.end(), 0);
-	}
-
-	template<typename T, typename... Args>
-	void _buildMessage(std::stringstream& ss, T first, Args... args) {
-		ss << first;
-		_buildMessage(ss, args...);
-	}
-
-	void _buildMessage(std::stringstream& ss) {
-
 	}
 
 public:
@@ -67,6 +58,7 @@ public:
 		save();
 	}
 
+	// Returns the content at the specified row.
 	std::string read(size_t row) const {
 		if (row >= _mapping.size()) {
 			throw std::out_of_range("FileManager::read: row out of bounds");
@@ -75,22 +67,24 @@ public:
 		return _content[_mapping[row]];
 	}
 
+	// Splits the content of the specified row by a delimiter and returns it.
 	std::vector<std::string> split(size_t row, char delimiter) const {
 		if (row >= _mapping.size()) {
 			throw std::out_of_range("FileManager::split: row out of bounds");
 		}
 
 		std::vector<std::string> result;
-		std::stringstream stream(_content[_mapping[row]]);
+		std::stringstream ss(_content[_mapping[row]]);
 		std::string part;
 
-		while (std::getline(stream, part, delimiter)) {
+		while (std::getline(ss, part, delimiter)) {
 			result.push_back(std::move(part));
 		}
 
 		return result;
 	}
 
+	// Returns the first row.
 	std::string first() const {
 		if (_mapping.size() == 0) {
 			throw std::out_of_range("FileManager::first: no rows present");
@@ -99,6 +93,7 @@ public:
 		return _content[_mapping[0]];
 	}
 
+	// Returns the last row.
 	std::string last() const {
 		if (_mapping.size() == 0) {
 			throw std::out_of_range("FileManager::last: no rows present");
@@ -107,6 +102,7 @@ public:
 		return _content[_mapping[_mapping.size() - 1]];
 	}
 
+	// Returns every row defined inside the file.
 	std::vector<std::string> all() const {
 		std::vector<std::string> result;
 		result.reserve(_mapping.size());
@@ -118,21 +114,32 @@ public:
 		return result;
 	}
 
-	void append(std::string content) {
-		_content.push_back(std::move(content));
+	// Appends given arguments to the file.
+	template<typename... Args>
+	void append(Args... args) {
+		std::stringstream ss;
+		(ss << ... << args);
+
+		_content.push_back(ss.str());
 		_mapping.push_back(_content.size() - 1);
 		++_appended;
 	}
-
-	void overwrite(size_t row, std::string content) {
+	
+	// Overwrites specified row with given arguments.
+	template<typename... Args>
+	void overwrite(size_t row, Args... args) {
 		if (row >= _mapping.size()) {
 			throw std::out_of_range("FileManager::overwrite: row out of bounds");
 		}
 
-		_content[_mapping[row]] = std::move(content);
+		std::stringstream ss;
+		(ss << ... << args);
+
+		_content[_mapping[row]] = ss.str();
 		_modified = true;
 	}
 
+	// Deletes the specified row.
 	void erase(size_t row) {
 		if (row >= _mapping.size()) {
 			throw std::out_of_range("FileManager::erase: row out of bounds");
@@ -152,12 +159,14 @@ public:
 		}
 	}
 
+	// Clears the file.
 	void clear() {
 		_mapping.clear();
 		_modified = true;
 		_appended = 0;
 	}
 
+	// Saves all changes.
 	void save() {
 		if (_modified) {
 			std::ofstream out(_path);
@@ -180,10 +189,12 @@ public:
 		}
 	}
 
+	// Returns number of present rows.
 	size_t size() const noexcept {
 		return _mapping.size();
 	}
 
+	// Returns true if the file is empty.
 	bool empty() const noexcept {
 		return _mapping.empty();
 	}
